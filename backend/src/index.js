81d04fcd53ea90dee9d98981e08166142dca1f3b
Heet Bhuva <heetbhuva1405@gmail.com>
2025-07-08 16:53:02 +0530

    cors resolved

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ['https://web3wallet-opal.vercel.app','http://localhost:5173'],
+  origin: 'https://web3wallet-opal.vercel.app',
   credentials: true,
 }));
 
